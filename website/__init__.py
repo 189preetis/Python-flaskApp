4eb9cb3188026d3423a8662ac8cf0174e4a10fa7
Preeti Singh <18preetigzp@gmail.com>
2025-05-10 22:10:23 +0530

    Initial commit

---
+++ website/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+    app.config['SECRET_KEY'] = os.getenv('secret_key')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
+    login_manager = LoginManager()
+    login_manager.loin_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import Users
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Users.query.get(int(user_id))
+
+    return app
